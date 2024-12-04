6d101b88d25cb6a2ad8efd4b00358f4f96b70c93
vcasbayprog <vcasbayprog@gmail.com>
2024-12-04 12:43:38 +0100

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Connect to MongoDB
+mongoose.connect(
+    process.env.MONGO
+)
+.then(()=>{
+    console.log('MongoDB connected successfully');
+}).catch((err)=>{
+    console.error('Error connecting to MongoDB:', err);
+});
 
 const app = express();
 
